3ae97fa85379936e0a7ee7ae3d0ccfa7fb710729
Erik LaBianca <erik.labianca@gmail.com>
2018-10-03 16:57:50 -0400

    CrudService should use TLogContext

--- slick/src/main/scala/com/theseventhsense/utils/services/CrudService.scala
+++ slick/src/main/scala/com/theseventhsense/utils/services/CrudService.scala
@@ -3,6 +3,7 @@ package com.theseventhsense.utils.services
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
+import com.theseventhsense.utils.models.TLogContext
 import com.theseventhsense.utils.persistence.QueryMeta
 import com.theseventhsense.utils.persistence.StreamQueryResult
 import com.theseventhsense.utils.persistence.db.BaseId
@@ -12,11 +13,11 @@ import com.theseventhsense.utils.persistence.db.Identified
   * Created by erik on 12/22/16.
   */
 trait CrudService[Id <: BaseId, T <: Identified[Id, T]] {
-  def get(id: Id)(implicit ec: ExecutionContext): Future[Option[T]]
-  def create(t: T)(implicit ec: ExecutionContext): Future[T]
-  def save(t: T)(implicit ec: ExecutionContext): Future[T]
-  def delete(id: Id)(implicit ec: ExecutionContext): Future[Int]
+  def get(id: Id)(implicit ec: ExecutionContext, lc: TLogContext): Future[Option[T]]
+  def create(t: T)(implicit ec: ExecutionContext, lc: TLogContext): Future[T]
+  def save(t: T)(implicit ec: ExecutionContext, lc: TLogContext): Future[T]
+  def delete(id: Id)(implicit ec: ExecutionContext, lc: TLogContext): Future[Int]
   def stream(meta: QueryMeta)(
-    implicit ec: ExecutionContext
+    implicit ec: ExecutionContext, lc: TLogContext
   ): Future[StreamQueryResult[T]]
 }
